b6dc31a0847db4e64252af377238a565c8244ec1
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-01-26 15:00:05 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
+var image;
+
 function upload() {
     var imgCanvas = document.getElementById("canvas1");
     var fileInput = document.getElementById("input-file");
     // var fileName = fileInput.value;
     // alert("Chose" + fileName);
-    var image = new SimpleImage(fileInput);
+    image = new SimpleImage(fileInput);
+    image.drawTo(imgCanvas);
+}
+
+function makeGrey() {
+    for (var pixel of image.values()) {
+        var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+        pixel.setRed(avg);
+        pixel.setGreen(avg);
+        pixel.setBlue(avg);
+    }
+    var imgCanvas = document.getElementById("canvas2");
     image.drawTo(imgCanvas);
 }
