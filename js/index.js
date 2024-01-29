6cc59e69a93fd9577b605f61ea45b94ee88cd157
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-01-29 12:58:05 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 var image;
 var greyImage;
+var fgImage = null;
+var bgImage = null;
+var canvasFg = document.getElementById("fgCan");
+var canvasBg = document.getElementById("bgCan");
+var greenThreshold = 240;
 
 function upload() {
     var imgCanvas = document.getElementById("canvas1");
@@ -10,7 +15,7 @@ function upload() {
     image = new SimpleImage(fileInput);
     greyImage = new SimpleImage(fileInput);
     image.drawTo(imgCanvas);
-    greyImage.drawTo()
+    greyImage.drawTo();
 }
 
 function makeGrey() {
@@ -23,3 +28,48 @@ function makeGrey() {
     var imgCanvas = document.getElementById("canvas2");
     image.drawTo(imgCanvas);
 }
+
+function loadForegroundImage() {
+    var imgFile = document.getElementById("fgFile");
+    fgImage = new SimpleImage(imgFile);
+    fgImage.drawTo(canvasFg);
+}
+
+function loadBackgroundImage() {
+    var imgFile = document.getElementById("bgFile");
+    bgImage = new SimpleImage(imgFile);
+    bgImage.drawTo(canvasBg);
+}
+
+function doGreenScreen() {
+    if (fgImage === null || ! fgImage.complete()) {
+        alert("Foreground not loaded");
+        return;
+    }
+    if (bgImage === null || ! bgImage.complete()) {
+        alert("Background not loaded");
+        return;
+    }
+    var output = new SimpleImage(fgImage.getWidth(), fgImage.getHeight());
+        for (var pixel of fgImage.values()) {
+        var x = pixel.getX();
+        var y = pixel.getY();
+        if (pixel.getGreen() > greenThreshold) {
+            var bgPixel = bgImage.getPixel(x, y);
+            output.setPixel(x, y, bgPixel);
+        }
+        else {
+            output.setPixel(x, y, pixel);
+        }
+    }
+    // output.drawTo(canvasFg);
+}
+
+function clearCanvas() {
+    var contextFg = canvasFg.getContext("2d");
+    contextFg.clearRect(0, 0, canvasFg.width, canvasFg.height);
+    var contextBg = canvasBg.getContext("2d");
+    contextBg.clearRect(0, 0, canvasBg.width, canvasBg.height);
+}
+
+
